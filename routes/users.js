f8f86f4f62720bda15c738ae8e40484c15790c98
Unknown <c12406518@mydit.ie>
2017-07-15 19:29:40 +0100

    err msg

--- routes/users.js
+++ routes/users.js
@@ -54,8 +54,8 @@ router.post('/register', function(req, res){
 
 		User.getUserByUsername(username, function(err, user){
     	if(err) throw err;
-    	if(!user){
-    		return done(null, false, {message: 'Unknown User'});
+    	if(user){
+    		return done(null, false, {message: 'Username is already taken'});
     	}
 		User.createUser(newUser, function(err, user){
 			if(err) throw err;
