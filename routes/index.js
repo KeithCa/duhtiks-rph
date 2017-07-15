83053cd22eb66d2a467ec0936c391ca0f7297ed6
Unknown <c12406518@mydit.ie>
2017-07-15 14:25:46 +0100

    test4
    sick of these tests

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,13 @@ var router = express.Router();
 
 // Get Homepage
 router.get('/', ensureAuthenticated, function(req, res){
-	res.render('index');
+	res.render('index', {
+	username : req.user.name // get the user out of session and pass to template
+});
 	console.log("Trying to figure out session variables here" + req.user.name);
 
 	//this holds the full session variables to get user name it is be req.user.id
 });
-
-var x = document.getElementsByTagName("p");
-x.innerHTML = "Hello," + req.user.name;
 
 function ensureAuthenticated(req, res, next){
 	if(req.isAuthenticated()){
