412803a3c1bcb08f2a6cad30f4395a84de0f1d96
Unknown <c12406518@mydit.ie>
2017-07-16 00:30:21 +0100

    added find by email func

--- models/user.js
+++ models/user.js
@@ -39,6 +39,11 @@ module.exports.getUserByUsername = function(username, callback){
 	User.findOne(query, callback);
 };
 
+module.exports.getUserByEmail = function(email, callback){
+	var query = {email: email};
+	User.findOne(query, callback);
+};
+
 
 module.exports.getUserById = function(id, callback){
 	User.findById(id, callback);
