ff60358511003a8b8d676d45a210b495bb76fc84
Unknown <duhtiks@gmail.com>
2017-07-19 13:19:56 +0300

    Partial grid

--- routes/location.js
+++ routes/location.js
@@ -1,19 +1,28 @@
 var express = require('express');
 var router = express.Router();
-
-
+var colors = {};
+var model = require('../models/loc_model');
 router.get('/', function(req, res){
 	res.render('location', {
 });
 var io = req.app.get('socketio');
 io.on('connection', function(socket){
   console.log('hello');
+  socket.on('getMap', function(){
+    socket.emit('heresTheMap', {map: model.map});
+  });
+  
+  
+  
 
 });
 
 
+
 });
 
 
+ 
+  
 
 module.exports = router;
